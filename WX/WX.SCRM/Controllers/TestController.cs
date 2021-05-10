7453d51806078c1078194f557d6ce6ddcbf007e1
youlo0018 <2553095849@qq.com>
2021-05-10 14:28:47 +0800

    ..

--- WX/WX.SCRM/Controllers/TestController.cs
+++ WX/WX.SCRM/Controllers/TestController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace WX.SCRM.Controllers
 {
+    [Produces("application/json")] //返回数据的格式 直接约定为Json
+    [Route("api/[controller]/[action]")]  //路由
     public class TestController : Controller
     {
         [HttpGet]
