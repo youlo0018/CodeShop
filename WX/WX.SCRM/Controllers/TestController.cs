17d1f7c5aec7c41808a265f8faf948782358bcde
youlo0018 <2553095849@qq.com>
2021-05-10 15:13:43 +0800

    ..

--- WX/WX.SCRM/Controllers/TestController.cs
+++ WX/WX.SCRM/Controllers/TestController.cs
@@ -7,7 +7,9 @@ using WX.SCRM.Uilt;
 
 namespace WX.SCRM.Controllers
 {
- 
+    /// <summary>
+    /// 测试控制器
+    /// </summary>
     public class TestController : BaseController
     {
         /// <summary>
@@ -21,9 +23,27 @@ namespace WX.SCRM.Controllers
 
         }
         [HttpPost]
-        public string TestPostSwagger()
+        public string TestPostSwagger(MovieModel movie)
         {
             return "1";
+        }
+        /// <summary>
+        /// 这是电影实体类
+        /// </summary>
+        public class MovieModel
+        {
+            /// <summary>
+            /// Id
+            /// </summary>
+            public int Id { get; set; }
+            /// <summary>
+            /// 影片名称
+            /// </summary>
+            public string Name { get; set; }
+            /// <summary>
+            /// 类型
+            /// </summary>
+            public string Type { get; set; }
         }
     }
 }
--- WX/WX.SCRM/Startup.cs
+++ WX/WX.SCRM/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,6 +16,10 @@ namespace WX.SCRM
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="services"></param>
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSwaggerGen(c =>
@@ -27,6 +32,11 @@ namespace WX.SCRM
                     Contact = new OpenApiContact { Name = "Youlo0018", Email = "", Url = new Uri("http://baidu.com") },
                     License = new OpenApiLicense { Name = "Youlo0018许可证", Url = new Uri("http://baidu.com") }
                 });
+                var basePath = Path.GetDirectoryName(typeof(Program).Assembly.Location);//获取应用程序所在目录（绝对，不受工作目录影响，建议采用此方法获取路径）
+                                                                                        //var basePath = AppContext.BaseDirectory;
+                var xmlPath = Path.Combine(basePath, "WX.SCRM.xml");//这个就是刚刚配置的xml文件名
+                c.IncludeXmlComments(xmlPath, true);//默认的第二个参数是false,对方法的注释
+                // c.IncludeXmlComments(xmlPath,true); //这个是controller的注释
             });
             services.AddControllers();
         }
@@ -37,19 +47,22 @@ namespace WX.SCRM
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                #region 开发环境显示swagger
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint($"/swagger/V1/swagger.json", $"XUnit.Core V1");
+                    c.RoutePrefix = string.Empty;     //如果是为空 访问路径就为 根域名/index.html,注意localhost:8001/swagger是访问不到的
+                                                      //路径配置，设置为空，表示直接在根域名（localhost:8001）访问该文件
+                                                      // c.RoutePrefix = "swagger"; // 如果你想换一个路径，直接写名字即可，比如直接写c.RoutePrefix = "swagger"; 则访问路径为 根域名/swagger/index.html
+                });
+                #endregion
             }
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint($"/swagger/V1/swagger.json", $"XUnit.Core V1");
-                c.RoutePrefix = string.Empty;     //如果是为空 访问路径就为 根域名/index.html,注意localhost:8001/swagger是访问不到的
-                //路径配置，设置为空，表示直接在根域名（localhost:8001）访问该文件
-                // c.RoutePrefix = "swagger"; // 如果你想换一个路径，直接写名字即可，比如直接写c.RoutePrefix = "swagger"; 则访问路径为 根域名/swagger/index.html
-            });
+
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
-            
+
                 endpoints.MapControllers();
             });
 
