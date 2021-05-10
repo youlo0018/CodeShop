37e43f25fdbbfaee55a1c78d51bce66098bb4ecf
youlo0018 <2553095849@qq.com>
2021-05-10 14:04:22 +0800

    ..

---
+++ WX/Common/Uilt/AllEumn.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Common.Uilt
+{
+    public enum ResponseCode
+    {
+        #region 公共返回码
+        /// <summary>
+        /// 请求成功
+        /// </summary>
+        [Description("请求成功")]
+        请求成功 = 10000,
+        /// <summary>
+        /// 请求处理中
+        /// </summary>
+        [Description("请求处理中")]
+        请求处理中 = 10001,
+        /// <summary>
+        /// 请求已受理
+        /// </summary>
+        [Description("请求已受理")]
+        请求已受理 = 10002,
+        /// <summary>
+        /// 请求失败
+        /// </summary>
+        [Description("请求失败")]
+        请求失败 = 10003,
+        /// <summary>
+        /// 查询失败
+        /// </summary>
+        [Description("查询失败")]
+        查询失败 = 10005,
+        /// <summary>
+        /// 交易关闭
+        /// </summary>
+        [Description("交易关闭")]
+        交易关闭 = 10006,
+        /// <summary>
+        /// 记录不存在
+        /// </summary>
+        [Description("记录不存在")]
+        记录不存在 = 10007,
+        /// <summary>
+        /// 用户不存在
+        /// </summary>
+        [Description("用户不存在")]
+        用户不存在 = 10008,
+        /// <summary>
+        /// 系统超时
+        /// </summary>
+        [Description("系统超时")]
+        系统超时 = 10098,
+        /// <summary>
+        /// 系统异常
+        /// </summary>
+        [Description("系统异常")]
+        系统异常 = 10099,
+        /// <summary>
+        /// 并发异常
+        /// </summary>
+        [Description("并发异常")]
+        并发异常 = 10097,
+        /// <summary>
+        /// 系统繁忙
+        /// </summary>
+        [Description("系统繁忙")]
+        系统繁忙 = 10096,
+        /// <summary>
+        /// 请求参数非法
+        /// </summary>
+        [Description("请求参数非法")]
+        请求参数非法 = 10100,
+        /// <summary>
+        /// 验证签名失败
+        /// </summary>
+        [Description("验证签名失败")]
+        验证签名失败 = 10102,
+        /// <summary>
+        /// 无权限
+        /// </summary>
+        [Description("无权限")]
+        无权限 = 10103,
+        #endregion
+
+        #region 私有返回码
+
+        #endregion
+    }
+}
--- WX/WX.SCRM/Startup.cs
+++ WX/WX.SCRM/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,18 @@ namespace WX.SCRM
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("V1", new OpenApiInfo
+                {
+                    Version = "V1",   //版本 
+                    Title = $"XUnit.Core 接口文档-NetCore3.1",  //标题
+                    Description = $"XUnit.Core Http API v1",    //描述
+                    Contact = new OpenApiContact { Name = "Youlo0018", Email = "", Url = new Uri("") },
+                    License = new OpenApiLicense { Name = "Youlo0018许可证", Url = new Uri("") }
+                });
+            });
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,16 +38,17 @@ namespace WX.SCRM
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint($"/swagger/V1/swagger.json", $"XUnit.Core V1");
+                c.RoutePrefix = string.Empty;     //如果是为空 访问路径就为 根域名/index.html,注意localhost:8001/swagger是访问不到的
+                //路径配置，设置为空，表示直接在根域名（localhost:8001）访问该文件
+                // c.RoutePrefix = "swagger"; // 如果你想换一个路径，直接写名字即可，比如直接写c.RoutePrefix = "swagger"; 则访问路径为 根域名/swagger/index.html
+            });
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+          
         }
     }
 }
